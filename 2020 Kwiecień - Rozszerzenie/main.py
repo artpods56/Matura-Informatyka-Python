7b7cbebcb65e967802e60053759a700a4d32c4c6
artpods56 <artpods56@gmail.com>
2021-09-06 23:52:24 +0200

    finished

--- 2020 Kwiecień - Rozszerzenie/main.py
+++ 2020 Kwiecień - Rozszerzenie/main.py
@@ -1,35 +1,28 @@
-plik = open("dane4.txt","r")
-ciagi = []
-luki = []
-for ciag in plik:
-    ciag = ciag.strip()
-    ciagi.append(ciag)
-#utworzenie tablicy ciagi
+def main():
+    plik = open("dane4.txt","r")
+    ciagi = []
+    luki = []
+    for ciag in plik:
+        ciag = ciag.strip()
+        ciagi.append(ciag)
 
-try:
-    i = 1
-    while i <= len(ciagi):
+
+    for i in range(1,len(ciagi)):
         r = int(ciagi[i])-int(ciagi[i-1])
         r = abs(r)
         luki.append(r)
-#        print(f"luka: {r} miedzy: {ciagi[i]} oraz: {ciagi[i-1]}")
-        i += 1
-except IndexError:
-    pass
-#utworzenie tablicy luk
 
-try:
-    i = 1
+    
     max_luka = 0
     min_luka = 100
     dl_luki = 1
     naj_dl_luki = 0
     start_luki = 0
     end_luki = 0
-    while i <= len(ciagi):
-#        print(f"luka: {luki[i-1]}  miedzy: {ciagi[i-1]} oraz: {ciagi[i]}")
+    for i in range(1,len(luki)):
+    #        print(f"luka: {luki[i-1]}  miedzy: {ciagi[i-1]} oraz: {ciagi[i]}")
 
-#       najdluzsza i najkrotsza luka ciagow
+    #       najdluzsza i najkrotsza luka ciagow
         if luki[i-1] > max_luka:
             max_luka = luki[i-1]
         if luki[i-1] < min_luka:
@@ -39,7 +32,7 @@ try:
             dl_luki += 1
 
         if luki[i] != luki[i-1] or luki[i] == luki[-1] :
-#           print(f' koniec i start: {ciagi[i]} {ciagi[i - dl_luki]}')
+    #           print(f' koniec i start: {ciagi[i]} {ciagi[i - dl_luki]}')
             if dl_luki > naj_dl_luki:
                 end_luki = ciagi[i]
                 start_luki = ciagi[i-dl_luki]
@@ -53,16 +46,15 @@ try:
         r_occurances = max(luki, key=luki.count)
         r_count = luki.count(r_occurances)
 
-except IndexError:
-    pass
-print("\nZadanie 4.1")
-print(f"najdluzsza luka: {max_luka}")
-print(f"najkrotsza luka: {min_luka}")
 
-print("\nZadanie 4.2")
-print(f"najdluzszy ciag regularny: {naj_dl_luki}")
-print(f"poczatek: {start_luki} koniec: {end_luki}")
+    plik4_1 = open("wynik4_1.txt","w")
+    plik4_1.truncate()
 
-print("\nZadanie 4.3")
-print(f"najczestsza wartosc luki: {r_occurances}")
-print(f"najczestsza krotność luki: {r_count}")
+
+    plik4_1.write(f"Zadanie 4.1 \nnajdłuższa luka: {max_luka} \n najkrótsza luka {min_luka}\n")
+    plik4_1.write(f"Zadanie 4.2 \nnajdłuższy ciąg regularny: {naj_dl_luki} \npoczątek: {start_luki}\nkoniec luki: {end_luki}\n")
+    plik4_1.write(f"Zadanie 4.3 \nnajczęstsza wartość luki: {r_occurances}\nnajczęstsza krotność luki: {r_count}")
+
+
+if __name__ == "__main__":
+    main()
--- 2021 Marzec - Rozszerzenie/main.py
+++ 2021 Marzec - Rozszerzenie/main.py
@@ -1,77 +1,80 @@
-plik = open("galerie.txt","r")
-dane = []
-for linia in plik:
-    linia = linia.strip()
-    linia = linia.split()
-    dane.append(linia)
+def main():
+    plik = open("galerie.txt","r")
+    dane = []
+    for linia in plik:
+        linia = linia.strip()
+        linia = linia.split()
+        dane.append(linia)
 
-liczba_miast = []
+    liczba_miast = []
 
-for kraj in dane:
-    if [kraj[0],0] not in liczba_miast:
-        liczba_miast.append([kraj[0],0])
+    for kraj in dane:
+        if [kraj[0],0] not in liczba_miast:
+            liczba_miast.append([kraj[0],0])
 
-for kraj in dane:
+    for kraj in dane:
+        for miasto in liczba_miast:
+            if kraj[0] == miasto[0]:
+                miasto[1] += 1
+
+
+    miasto_powierzchnia_lokale = []
+
+    def p_powierzchni(x,y):
+        p = x*y
+        return p
+
+    i = 0
+    max = ["",0]
+    min = ["",100000]
+    min_lokale = ["",100]
+    max_lokale = ["",0]
+
+    for miasto in dane:
+        
+        suma_p = 0
+        liczba_lokali = 0
+        pola_lokali = []
+
+        for i in range(2,len(miasto),2):
+            if int(miasto[i]) != 0 and int(miasto[i+1]) != 0:
+                suma_p += p_powierzchni(int(miasto[i]),int(miasto[i+1]))
+                liczba_lokali += 1
+                p = p_powierzchni(int(miasto[i]),int(miasto[i+1]))
+                if p not in pola_lokali:
+                    pola_lokali.append(p)
+        if suma_p > max[1]:
+            max[0] = miasto[1]
+            max[1] = suma_p
+        if suma_p < min[1]:
+            min[0] = miasto[1]
+            min[1] = suma_p
+        if len(pola_lokali) > max_lokale[1]:
+            max_lokale[0] = miasto[1]
+            max_lokale[1] = len(pola_lokali)
+        if len(pola_lokali) < min_lokale[1]:
+            min_lokale[0] = miasto[1]
+            min_lokale[1] = len(pola_lokali)
+        miasto_powierzchnia_lokale.append([miasto[1],suma_p,liczba_lokali])
+
+
+    plik4_1 = open("wynik4_1.txt","w")
+    plik4_1.truncate()
     for miasto in liczba_miast:
-        if kraj[0] == miasto[0]:
-            miasto[1] += 1
+        plik4_1.write(f"{miasto} \n")
 
+    plik4_2a = open("wynik4_2a.txt","w")
+    plik4_2a.truncate()
+    for miasto in miasto_powierzchnia_lokale:
+        plik4_2a.write(f"{miasto} \n")
 
-miasto_powierzchnia_lokale = []
+    plik4_2b = open("wynik4_2b.txt","w")
+    plik4_2b.truncate()
+    plik4_2b.write(f"Miasto o najmniejszej powierzchni całkowitej lokali: {min} \nMiasto o największej powierzchni całkowitej lokali: {max}")
 
-def p_powierzchni(x,y):
-    p = x*y
-    return p
-
-i = 0
-max = ["",0]
-min = ["",100000]
-min_lokale = ["",100]
-max_lokale = ["",0]
-
-for miasto in dane:
-    
-    suma_p = 0
-    liczba_lokali = 0
-    pola_lokali = []
-
-    for i in range(2,len(miasto),2):
-        if int(miasto[i]) != 0 and int(miasto[i+1]) != 0:
-            suma_p += p_powierzchni(int(miasto[i]),int(miasto[i+1]))
-            liczba_lokali += 1
-            p = p_powierzchni(int(miasto[i]),int(miasto[i+1]))
-            if p not in pola_lokali:
-                pola_lokali.append(p)
-    if suma_p > max[1]:
-        max[0] = miasto[1]
-        max[1] = suma_p
-    if suma_p < min[1]:
-        min[0] = miasto[1]
-        min[1] = suma_p
-    if len(pola_lokali) > max_lokale[1]:
-        max_lokale[0] = miasto[1]
-        max_lokale[1] = len(pola_lokali)
-    if len(pola_lokali) < min_lokale[1]:
-        min_lokale[0] = miasto[1]
-        min_lokale[1] = len(pola_lokali)
-    miasto_powierzchnia_lokale.append([miasto[1],suma_p,liczba_lokali])
-
-
-
-plik4_1 = open("wynik4_1.txt","w")
-plik4_1.truncate()
-for miasto in liczba_miast:
-    plik4_1.write(f"{miasto} \n")
-
-plik4_2a = open("wynik4_2a.txt","w")
-plik4_2a.truncate()
-for miasto in miasto_powierzchnia_lokale:
-    plik4_2a.write(f"{miasto} \n")
-
-plik4_2b = open("wynik4_2b.txt","w")
-plik4_2b.truncate()
-plik4_2b.write(f"Miasto o najmniejszej powierzchni całkowitej lokali: {min} \nMiasto o największej powierzchni całkowitej lokali: {max}")
-
-plik4_3 = open("wynik4_3.txt","w")
-plik4_3.truncate()
-plik4_3.write(f"Miasto o największej ilości różnych lokali: {max_lokale} \nMiasto o najmnijeszej ilości różnych lokali: {min_lokale}")
+    plik4_3 = open("wynik4_3.txt","w")
+    plik4_3.truncate()
+    plik4_3.write(f"Miasto o największej ilości różnych lokali: {max_lokale} \nMiasto o najmnijeszej ilości różnych lokali: {min_lokale}")
+    print("gowno")
+if __name__ == "__main__":
+    main()
